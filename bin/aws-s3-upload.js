cc1825524bf4021eb62c770fc71304078a21bd3a
MicFin <mrfinneran@gmail.com>
2017-06-21 11:18:15 -0400

    Require aws-sdk and fs in script
    To ensure our aws-sdk is required correctly we will try to pass in
    our image and upload it.  `.upload` should fail but be recognized.

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -1,1 +1,18 @@
 'use strict'
+
+// require aws-sdk module
+const AWS = require('aws-sdk')
+
+// create a new instance of AWS.S3 object
+const s3 = new AWS.S3()
+
+// require fs module
+const fs = require('fs')
+
+// script should accept file as 2nd argument
+// print to make sure it does
+console.log("file you're uploading is ", process.argv[2])
+
+// attempt s3.upload knowing it will fail to ensure the
+// module is required correctly
+s3.upload(process.argv[2])
