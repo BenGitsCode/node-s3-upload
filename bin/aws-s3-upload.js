a0dec2d52648d47827f97847ffd4fdb7577c326c
Ben Adamski <benjadamski@gmail.com>
2017-06-22 12:54:51 -0400

    Complete s3.upload params and upload image file
    To build out the correct params object to send to .upload
    Use node fs module to create a read stream
    Use ENV to set Bucket
    Pass params and define a callback for the request
    
    Upload should now work and the file should be on AWS
    after running `node bin/aws-s3-upload images/programmersLife.png`

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -3,14 +3,14 @@
 // This loads the env variables to a process.env object
 require('dotenv').load()
 
+// require fs module
+const fs = require('fs')
+
 // require aws-sdk module
 const AWS = require('aws-sdk')
 
 // create a new instance of AWS.S3 object
 const s3 = new AWS.S3()
-
-// require fs module
-const fs = require('fs')
 
 // testing that dotenv module works and loads bucket name
 console.log('bucket to be uploaded to is ' + process.env.AWS_S3_BUCKET_NAME)
@@ -18,6 +18,26 @@ console.log('bucket to be uploaded to is ' + process.env.AWS_S3_BUCKET_NAME)
 // print to make sure it does
 console.log('File to be uploaded ', process.argv[2])
 
+// use node fs module to create a read stream
+// for our image file
+// https://www.sitepoint.com/basics-node-js-streams/
+const stream = fs.createReadStream(process.argv[2])
+
+console.log('Stream is: ' + stream)
+
+const params = {
+  ACL: 'public-read',
+  Bucket: process.env.AWS_S3_BUCKET_NAME,
+  Body: stream,
+  Key: 'with'
+}
+
 // attempt s3.upload knowing it will fail to ensure the
 // module is required correctly
-s3.upload(process.argv[2])
+s3.upload(params, function (error, data) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.log(data)
+  }
+})
